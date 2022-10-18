70f42c40d7a979ad85c576d3d3f51283074fcfe2
NimuCrybaby <kocsis.vikino@gmail.com>
2022-10-18 15:56:11 +0200

    New UI

--- Monmusu Rancher/Assets/Scripts/FarmUI.cs
+++ Monmusu Rancher/Assets/Scripts/FarmUI.cs
@@ -7,14 +7,26 @@ public class FarmUI : MonoBehaviour
 {
     public GameObject MilkFactoryPanel;
     public GameObject WoolFactoryPanel;
+    public GameObject DefaultPanel;
 
     public void OpenMilkFactory(){
         MilkFactoryPanel.SetActive(true);
         WoolFactoryPanel.SetActive(false);
+        DefaultPanel.SetActive(false);
     }
 
     public void OpenWoolFactory(){
         WoolFactoryPanel.SetActive(true);
         MilkFactoryPanel.SetActive(false);
+        DefaultPanel.SetActive(false);
+    }
+
+    public void CloseMilkFactory(){
+        MilkFactoryPanel.SetActive(false);
+        DefaultPanel.SetActive(true);
+    }
+    public void CloseWoolFactory(){
+        WoolFactoryPanel.SetActive(false);
+        DefaultPanel.SetActive(true);
     }
 }
